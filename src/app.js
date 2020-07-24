8edc8bd829597de92903f22433fc77bc94030a69
Siyath Kasali <jsiyath@gmail.com>
2020-07-24 21:28:48 +0530

    installed syyled compoents modifiers

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { PrimaryButton, SecondaryButton, TertiaryButton } from './components/Buttons';
+import { GlobalStyle } from './utils/Global';
 
-export default function app() {
+export default function App() {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <PrimaryButton modifiers="small">Hello World</PrimaryButton>
+      <SecondaryButton modifiers={["large", "warning", "secondaryButtonWarning"]}>Hello World</SecondaryButton>
+      <TertiaryButton>Hello World</TertiaryButton>
+      <GlobalStyle />
     </div>
   );
 }
---
+++ src/components/Buttons.js
@@ -0,0 +1,141 @@
+import { defaultTheme } from '../utils/themes';
+import { typeScale } from '../utils/typography';
+import styled from 'styled-components';
+import { applyStyleModifiers } from "styled-components-modifiers";
+
+const BUTTON_MODIFIERS = {
+  small: () => `
+    font-size: ${typeScale.helperText};
+    padding: 8px;
+  `,
+  large: () => `
+    font-size: ${typeScale.header5};
+    padding: 16px 24px;
+  `,
+  warning: () => `
+    background: none;
+    color: ${defaultTheme.status.warningColor};
+    &:hover, &:focus {
+      background-color: ${defaultTheme.status.warningColorHover};
+      outline: 3px solid ${defaultTheme.status.warningColorHover};
+      outline-offset: 2px;
+      border: 2px solid transparent;
+    }
+
+    &:active {
+      background-color: ${defaultTheme.status.warningColorActive};
+    }
+  `,
+  primaryButtonWarning: () => `
+    background-color: ${defaultTheme.status.warningColor};
+    color: ${defaultTheme.textColorInverted};
+  `,
+  secondaryButtonWarning: () => `
+    border: 2px solid ${defaultTheme.status.warningColor};
+  `,
+  error: () => `
+    background: none;
+    color: ${defaultTheme.status.errorColor};
+    &:hover, &:focus {
+      background-color: ${defaultTheme.status.errorColorHover};
+      outline: 3px solid ${defaultTheme.status.errorColorHover};
+      outline-offset: 2px;
+      border: 2px solid transparent;
+    }
+    &:active {
+      background-color: ${defaultTheme.status.errorColorActive};
+    }
+  `,
+  primaryButtonError: () => `
+    background-color: ${defaultTheme.status.errorColor};
+    color: ${defaultTheme.textColorInverted};
+  `,
+  secondaryButtonError: () => `
+    border: 2px solid ${defaultTheme.status.warningColor};
+  `,
+  success: () => `
+    background: none;
+    color: ${defaultTheme.status.successColor};
+    &:hover, &:focus {
+      background-color: ${defaultTheme.status.successColorHover};
+      outline: 3px solid ${defaultTheme.status.successColorHover};
+      outline-offset: 2px;
+      border: 2px solid transparent;
+    }
+    &:active {
+      background-color: ${defaultTheme.status.successColorActive};
+    }
+  `,
+  primaryButtonSuccess: () => `
+    background-color: ${defaultTheme.status.successColor};
+    color: ${defaultTheme.textColorInverted};
+  `,
+  secondaryButtonSuccess: () => `
+    border: 2px solid ${defaultTheme.status.warningColor};
+  `
+};
+
+
+export const Button = styled.button`
+  border-radius: 2px;
+  padding: 12px 24px;
+  font-size: ${typeScale.paragraph};
+  cursor: pointer;
+  font-family: ${defaultTheme.primaryFont};
+  transition: background-color 0.2s linear, color 0.2s linear;
+
+  &:hover {
+    background-color: ${defaultTheme.primaryHoverColor};
+    color: ${defaultTheme.textColorOnPrimary};
+  }
+  &:focus {
+    outline: 3px solid ${defaultTheme.primaryHoverColor};
+    outline-offset: 2px;
+  }
+
+  &:active {
+    background-color: ${defaultTheme.primaryActiveColor};
+    border-color: ${defaultTheme.primaryActiveColor};
+    color: ${defaultTheme.textColorOnPrimary};
+  }
+
+  &:disabled {
+    cursor: not-allowed;
+    background: none;
+    color: ${defaultTheme.disabled};
+    border: none;
+  }
+`;
+
+export const PrimaryButton = styled(Button)`
+  background-color: ${defaultTheme.primaryColor};
+  color: ${defaultTheme.textColorOnPrimary};
+  border: 2px solid transparent;
+
+  &:disabled {
+    background-color: ${defaultTheme.disabled};
+    color: ${defaultTheme.textOnDisabled};
+    border-color: ${defaultTheme.disabled};
+  }
+
+  ${applyStyleModifiers(BUTTON_MODIFIERS)}
+`;
+
+export const SecondaryButton = styled(Button)`
+  border: 2px solid ${defaultTheme.primaryColor};
+  color: ${defaultTheme.primaryColor};
+  background: none;
+
+  &:disabled {
+    border: 2px solid ${defaultTheme.disabled};
+    border-color: ${defaultTheme.disabled};
+  }
+   
+  ${applyStyleModifiers(BUTTON_MODIFIERS)}
+`;
+
+export const TertiaryButton = styled(Button)`
+  border: 2px solid transparent;
+  color: ${defaultTheme.primaryColor};
+  background: none;
+`;
